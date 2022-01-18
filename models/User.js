a4f69c5598fd316726138c888db36fd03966436c
wwbarton <wbartonw5@gmail.com>
2022-01-17 17:49:59 -0700

    models

---
+++ models/User.js
@@ -0,0 +1,52 @@
+const { Schema, model } = require("mongoose");
+
+const UserSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: "We need to know what to call you, please enter a user name",
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: "Please enter a valid email address",
+      unique: true,
+      validate: {
+        validator(validEmail) {
+          return /^([a-zA-Z0-9_\.-]+)@([\da-z\.-]+)\.([a-z]{2,6})(\.[a-z]{2,6})?$/.test(
+            validEmail
+          );
+        },
+        message: "Please enter a valid email address",
+      },
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+UserSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
+});
+
+const User = model("User", UserSchema);
+
+module.exports = User;
